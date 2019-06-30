910588841b305f00472bc0c0e2c3b459a9fe6484
philomath213 <bilalphilomath@gmail.com>
2019-06-30 16:57:12 +0100

    update packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Inject, Renderer, ElementRef, ViewChild, HostListene
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/filter';
-import { DOCUMENT } from '@angular/platform-browser';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
+
+import { LocationStrategy, PlatformLocation, Location, DOCUMENT } from '@angular/common';
 
 var didScroll;
 var lastScrollTop = 0;
